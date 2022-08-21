ba3a77538ea7d980b0f6b003164349533a289f2f
wsy <wsy8718@163.com>
2022-08-21 23:26:00 +0800

    sql: 创建user,post表

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 
 from app import db 
+from datetime import datetime 
 
 
 class User(db.Model):
@@ -7,6 +8,20 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
 
     def __repr__(self) -> str:
         return "<User {}>".format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+    
+        
+
