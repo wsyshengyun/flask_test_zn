a4becc5ff9bd6f4bc6b17d72adb2622b8b20f6f6
wsy <wsy8718@163.com>
2022-08-21 15:34:38 +0800

    ...

--- routes.py
+++ routes.py
@@ -1,16 +1,21 @@
 from app import app 
-from flask import render_template 
+from flask import render_template , flash, redirect
 from forms import LoginForm 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm() 
+    if form.validate_on_submit():
+        flash("Login requested for user{}, remember_me={}".format(
+            form.username.data, form.remember_me.data
+        ))     
+        return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
 
-
+@app.route('/index')
 @app.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 from flask import current_app
 current_app.logger.debug('routes.py')
